810576c73464bcf1af09c617b4459d44bc8cda4e
dvlapo <agbooladolapo24@gmail.com>
2026-06-02 16:51:48 +0100

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,24 @@
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+  });
+
+  app.setGlobalPrefix('api/v1');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  await app.listen(process.env.PORT ?? 3333);
+  console.log(
+    `App running on: http://localhost:${process.env.PORT ?? 3333}/api/v1`,
+  );
+}
+bootstrap();
